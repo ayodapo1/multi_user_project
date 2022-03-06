04b3fecc1031be8c34bf39dfa1fa44513006db2d
Neba Nfonsang <neba.nfonsang@du.edu>
2022-03-06 06:52:48 -0700

    modified settings.py and blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,29 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1> This is the project home page </h1>")
+    return HttpResponse('<h1>This is the project page</h1>')
 
 def blog_home(request):
-    return HttpResponse("<h1> This is the blog's home page </h1>")
+    
+    all_posts = [
+        {
+            'author': 'James',
+            'title': 'Software Engineering',
+            'contents': 'Software engineering is great',
+            'date_posted': 'March 5, 2022'
+        },
+
+        {
+            'author': 'Jackie',
+            'title': 'DevOps Engineering on the Rise',
+            'contents': 'CI/CD pipeline, a must have skill!',
+            'date_posted': 'March 6, 2022'
+        }
+    ]
+
+    return render(request,"blog/home.html", {"posts": all_posts})
 
 def blog_about(request):
-    return HttpResponse("<h1> This is the blog's about page </h1>")
+    contents = "Hi, this blog is a platform for several users"
+    title = "About"
+    return render(request,"blog/about.html", {"about_contents":contents, "title":title})
